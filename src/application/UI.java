4c02f10d4abc8e63ee159066f62ad8f223663ef3
Gabriel Tiole <tiole62@gmail.com>
2021-08-24 16:16:37 -0300

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,9 +1,14 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
+
 
 	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
 
@@ -25,6 +30,18 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		}
+		catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+		}
+	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
@@ -32,15 +49,13 @@ public class UI {
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
-
 			System.out.println();
 		}
-
 		System.out.println("  a b c d e f g h");
 	}
 
-	public static void printPiece(ChessPiece piece) {
-		if (piece == null) {
+	private static void printPiece(ChessPiece piece) {
+    	if (piece == null) {
             System.out.print("-");
         }
         else {
@@ -51,7 +66,6 @@ public class UI {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
-
-		System.out.print(" ");
+        System.out.print(" ");
 	}
 }
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -3,22 +3,13 @@ package chess;
 import boardgame.Position;
 
 public class ChessPosition {
-<<<<<<< HEAD
-	private char column;
-	private int row;
-
-	public ChessPosition(char column, int row) {
-		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
-=======
 
 	private char column;
 	private int row;
 	
 	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");	
->>>>>>> 910533247fbd7e494a6eaca59d41fefda175710c
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
 		}
 		this.column = column;
 		this.row = row;
@@ -31,33 +22,17 @@ public class ChessPosition {
 	public int getRow() {
 		return row;
 	}
-<<<<<<< HEAD
 
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
-	}
-
-	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
-	}
-
-=======
-	
-	protected Position toPosition() {
-		return new Position(8 - row,  column - 'a');	
 	}
 	
 	protected static ChessPosition fromPosition(Position position) {
 		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
 	}
 	
->>>>>>> 910533247fbd7e494a6eaca59d41fefda175710c
 	@Override
 	public String toString() {
 		return "" + column + row;
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> 910533247fbd7e494a6eaca59d41fefda175710c
 }
